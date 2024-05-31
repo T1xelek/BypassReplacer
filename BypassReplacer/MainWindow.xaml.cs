28e7de54da075d2a3d2febab2d7a1079ba358f6a
Serega007 <ermaksochi@gmail.com>
2024-06-01 01:31:09 +0300

    Теперь BypassReplacer работает в любой папке (если пользователь изменил папку расположения игры кристаликса) Добавлены проверки на вшивость, проверка на существование Xenoceal папки, проверка на symlink в случае если последняя подмена была не удачная И многие другие мелкие правки

--- BypassReplacer/MainWindow.xaml.cs
+++ BypassReplacer/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
+using System.Management;
 using System.Runtime.InteropServices;
 using System.Text.RegularExpressions;
 using System.Threading;
@@ -59,8 +60,14 @@ namespace BypassReplacer
                 string replacePath = "\\Minigames\\";
                 string replaceName = "minecraft.jar";
 
+                if (!File.Exists("C:\\Xenoceal\\" + replaceName))
+                {
+                    Dispatcher.Invoke(() => inform.Text = "Ошибка: Не найден файл " + "C:\\Xenoceal\\" + replaceName + "\n\nПерезапустите BypassReplacer для повторной попытки");
+                    return;
+                }
+
                 Console.WriteLine("Запустите Cristalix...");
-                Dispatcher.Invoke(() => this.inform.Text = "Запустите Cristalix...");
+                Dispatcher.Invoke(() => inform.Text = "Запустите Cristalix...");
 
                 while (true)
                 {
@@ -72,10 +79,24 @@ namespace BypassReplacer
                         foreach (Process p in process)
                         {
                             string path = p.MainModule.FileName;
+                            string cmdLine = GetCommandLine(p);
 
-                            if (!path.Contains("\\.cristalix\\")) continue;
+                            if (!cmdLine.Contains("ru.cristalix")) continue;
 
-                            string pathSunEc = path.Replace("\\23-jre-win-64\\bin\\java.exe", replacePath + replaceName);
+                            /*try
+                            {
+                                Process pp = GetParent(p);
+                                if (pp != null)
+                                {
+                                    if (pp.ProcessName.Contains("javaw")) continue;
+                                }
+                            }
+                            catch (Exception)
+                            {
+                                continue;
+                            }*/
+
+                            string pathSunEc = path.Substring(0, path.IndexOf("\\updates\\")) + "\\updates" + replacePath + replaceName;
 
                             if (!File.Exists(pathSunEc)) continue;
                             if (currTime - new TimeSpan(p.StartTime.Ticks).TotalSeconds > 3)
@@ -85,35 +106,57 @@ namespace BypassReplacer
                             }
 
                             Console.WriteLine("Cristalix найден! ждёмс чудо...");
-                            Dispatcher.Invoke(() => this.inform.Text = "Cristalix найден! ждёмс чудо...");
+                            Dispatcher.Invoke(() => inform.Text = "Cristalix найден! ждёмс чудо...");
                             filePath = pathSunEc;
-                            filePathMinecraft = path.Substring(0, path.IndexOf("\\.cristalix\\")) + "\\.cristalix\\updates\\Minigames\\minecraft.jar";
+                            filePathMinecraft = path.Substring(0, path.IndexOf("\\updates\\")) + "\\updates\\Minigames\\minecraft.jar";
                             currProcess = p;
                         }
-                        if (filePath.Length > 0) break;
-                        Thread.Sleep(1);
+                        if (currProcess != null) break;
+                        Thread.Sleep(50);
                     }
                     catch (Exception ex)
                     {
                         Console.WriteLine(ex.ToString());
-                        Dispatcher.Invoke(() => this.inform.Text = $"Ошибка: {ex.Message}");
+                        Dispatcher.Invoke(() => inform.Text = $"Ошибка: {ex.Message}" + "\nПерезапустите BypassReplacer для повторной попытки");
                         break;
                     }
                 }
-                if (filePath.Length == 0) return;
+                if (currProcess == null) return;
                 string tempPath = System.IO.Path.Combine(System.IO.Path.GetTempPath(), "sunec_temp");
                 string tempFile = System.IO.Path.Combine(tempPath, replaceName);
+
+                Console.WriteLine("Проверка на вшивость...");
+                Dispatcher.Invoke(() => inform.Text = "Проверка на вшивость...");
+
+                if (!File.Exists("C:\\Xenoceal\\" + replaceName))
+                {
+                    Dispatcher.Invoke(() => inform.Text = "Ошибка: Не найден файл " + "C:\\Xenoceal\\" + replaceName + "\n\nПерезапустите BypassReplacer для повторной попытки");
+                    return;
+                }
+                if (IsSymbolicLink(filePath))
+                {
+                    File.Delete(filePath);
+                    Dispatcher.Invoke(() => inform.Text = "Ошибка: Похоже предыдущая попытка подмена была не успешной, перезапустите лаунчер (кристаликс) без BypassReplacer и попробуйте снова\n\nПерезапустите BypassReplacer для повторной попытки");
+                    return;
+                }
+
+                if (IsFileEqual("C:\\Xenoceal\\" + replaceName, filePath))
+                {
+                    Dispatcher.Invoke(() => inform.Text = "Ошибка: Похоже файл " + "C:\\Xenoceal\\" + replaceName + " был перезаписан лаунчером, верните модифиваронный " + replaceName + "\n\nПерезапустите BypassReplacer для повторной попытки");
+                    return;
+                }
+
                 try
                 {
                     Directory.CreateDirectory(tempPath);
                     File.Copy(filePath, tempFile, overwrite: true);
 
                     Console.WriteLine("Подмена 1...");
-                    Dispatcher.Invoke(() => this.inform.Text = "Подмена 1...");
+                    Dispatcher.Invoke(() => inform.Text = "Подмена 1...");
                     JavaProcess(currProcess, false);
                     if (multiProcess.Count > 0)
                     {
-                        Dispatcher.Invoke(() => this.inform.Text += "\n\nОбнаружено что запущено несколько майнкрафтов (кристаликса), они будут заморожены на момент инжекта во избежания краша");
+                        Dispatcher.Invoke(() => inform.Text += "\n\nОбнаружено что запущено несколько майнкрафтов (кристаликса), они будут заморожены на момент инжекта во избежания краша");
                     }
                     foreach (Process p in multiProcess)
                     {
@@ -129,11 +172,11 @@ namespace BypassReplacer
                     JavaProcess(currProcess, true);
 
                     Console.WriteLine("Ждём запуска майнкрафта...");
-                    Dispatcher.Invoke(() => this.inform.Text = "Ждём запуска майнкрафта...");
+                    Dispatcher.Invoke(() => inform.Text = "Ждём запуска майнкрафта...");
                     if (multiProcess.Count > 0)
                     {
                         Console.WriteLine("Обнаружено что запущено несколько майнкрафтов (кристаликса), они будут заморожены на момент инжекта во избежания краша");
-                        Dispatcher.Invoke(() => this.inform.Text += "\n\nОбнаружено что запущено несколько майнкрафтов (кристаликса), они будут заморожены на момент инжекта во избежания краша");
+                        Dispatcher.Invoke(() => inform.Text += "\n\nОбнаружено что запущено несколько майнкрафтов (кристаликса), они будут заморожены на момент инжекта во избежания краша");
                     }
                     while (true)
                     {
@@ -154,16 +197,16 @@ namespace BypassReplacer
                                 break;
                             }
                         }
-                        Thread.Sleep(10);
+                        Thread.Sleep(50);
                     }
 
                     Console.WriteLine("Подмена 2...");
-                    Dispatcher.Invoke(() => this.inform.Text = "Подмена 2...");
+                    Dispatcher.Invoke(() => inform.Text = "Подмена 2...");
                     JavaProcess(currProcess, false);
                     if (multiProcess.Count > 0)
                     {
                         Console.WriteLine("Обнаружено что запущено несколько майнкрафтов (кристаликса), они будут заморожены на момент инжекта во избежания краша");
-                        Dispatcher.Invoke(() => this.inform.Text += "\n\nОбнаружено что запущено несколько майнкрафтов (кристаликса), они будут заморожены на момент инжекта во избежания краша");
+                        Dispatcher.Invoke(() => inform.Text += "\n\nОбнаружено что запущено несколько майнкрафтов (кристаликса), они будут заморожены на момент инжекта во избежания краша");
                     }
                     File.Delete(filePath);
                     File.Copy(tempFile, filePath, overwrite: true);
@@ -178,7 +221,7 @@ namespace BypassReplacer
                         }
                     }
                     Console.WriteLine("Вроде всё прошло успешно, проверяйте");
-                    Dispatcher.Invoke(() => this.inform.Text = "Вроде всё прошло успешно, проверяйте");
+                    Dispatcher.Invoke(() => inform.Text = "Вроде всё прошло успешно, проверяйте");
                     Thread.Sleep(2000);
 
                     Dispatcher.Invoke(() => this.Close());
@@ -186,7 +229,7 @@ namespace BypassReplacer
                 catch (Exception ex)
                 {
                     Console.WriteLine(ex.ToString());
-                    Dispatcher.Invoke(() => this.inform.Text = $"Ошибка: {ex.Message}");
+                    Dispatcher.Invoke(() => inform.Text = $"Ошибка: {ex.Message}\nПерезапустите BypassReplacer для повторной попытки");
                     try
                     {
                         foreach (Process p in multiProcess)
@@ -196,6 +239,14 @@ namespace BypassReplacer
                             {
                                 JavaProcess(p, true);
                             }
+                        }
+                    }
+                    catch { }
+                    try
+                    {
+                        if (IsSymbolicLink(filePath))
+                        {
+                            File.Delete(filePath);
                         }
                     }
                     catch { }
@@ -218,26 +269,112 @@ namespace BypassReplacer
             }
         }
 
+        static bool IsSymbolicLink(string path)
+        {
+            FileInfo file = new FileInfo(path);
+            return file.Attributes.HasFlag(FileAttributes.ReparsePoint);
+        }
+
+        static bool IsFileEqual(string filepath1, string filepath2)
+        {
+            using (var reader1 = new System.IO.FileStream(filepath1, System.IO.FileMode.Open, System.IO.FileAccess.Read))
+            {
+                using (var reader2 = new System.IO.FileStream(filepath2, System.IO.FileMode.Open, System.IO.FileAccess.Read))
+                {
+                    byte[] hash1;
+                    byte[] hash2;
+
+                    using (var md51 = new System.Security.Cryptography.MD5CryptoServiceProvider())
+                    {
+                        md51.ComputeHash(reader1);
+                        hash1 = md51.Hash;
+                    }
+
+                    using (var md52 = new System.Security.Cryptography.MD5CryptoServiceProvider())
+                    {
+                        md52.ComputeHash(reader2);
+                        hash2 = md52.Hash;
+                    }
+
+                    int j = 0;
+                    for (j = 0; j < hash1.Length; j++)
+                    {
+                        if (hash1[j] != hash2[j])
+                        {
+                            break;
+                        }
+                    }
+
+                    return j == hash1.Length;
+                }
+            }
+        }
+
         private void JavaProcess(Process process, bool active)
         {
-            try
+            foreach (ProcessThread thread in process.Threads)
             {
-                foreach (ProcessThread thread in process.Threads)
+                IntPtr intPtr = OpenThread(ThreadAccess.SUSPEND_RESUME, bInheritHandle: false, (uint)thread.Id);
+                if (intPtr != IntPtr.Zero)
                 {
-                    IntPtr intPtr = OpenThread(ThreadAccess.SUSPEND_RESUME, bInheritHandle: false, (uint)thread.Id);
-                    if (intPtr != IntPtr.Zero)
+                    if (active) ResumeThread(intPtr);
+                    else SuspendThread(intPtr);
+                    CloseHandle(intPtr);
+                }
+            }
+        }
+
+        // Define an extension method for type System.Process that returns the command 
+        // line via WMI.
+        private static string GetCommandLine(Process process)
+        {
+            string cmdLine = null;
+            using (var searcher = new ManagementObjectSearcher(
+              $"SELECT CommandLine FROM Win32_Process WHERE ProcessId = {process.Id}"))
+            {
+                // By definition, the query returns at most 1 match, because the process 
+                // is looked up by ID (which is unique by definition).
+                using (var matchEnum = searcher.Get().GetEnumerator())
+                {
+                    if (matchEnum.MoveNext()) // Move to the 1st item.
                     {
-                        if (active) ResumeThread(intPtr);
-                        else SuspendThread(intPtr);
-                        CloseHandle(intPtr);
+                        cmdLine = matchEnum.Current["CommandLine"]?.ToString();
                     }
                 }
             }
-            catch (Exception ex)
+            if (cmdLine == null)
             {
-                Console.WriteLine(ex.ToString());
-                this.inform.Text = $"Ошибка: {ex.Message}";
+                // Not having found a command line implies 1 of 2 exceptions, which the
+                // WMI query masked:
+                // An "Access denied" exception due to lack of privileges.
+                // A "Cannot process request because the process (<pid>) has exited."
+                // exception due to the process having terminated.
+                // We provoke the same exception again simply by accessing process.MainModule.
+                var dummy = process.MainModule; // Provoke exception.
             }
+            return cmdLine;
         }
+
+        /*public static Process GetParent(Process process)
+        {
+            try
+            {
+                using (var query = new ManagementObjectSearcher(
+                  "SELECT * " +
+                  "FROM Win32_Process " +
+                  "WHERE ProcessId=" + process.Id))
+                {
+                    return query
+                      .Get()
+                      .OfType<ManagementObject>()
+                      .Select(p => Process.GetProcessById((int)(uint)p["ParentProcessId"]))
+                      .FirstOrDefault();
+                }
+            }
+            catch
+            {
+                return null;
+            }
+        }*/
     }
 }
